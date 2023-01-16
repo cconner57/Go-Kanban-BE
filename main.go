0afdbbf039965bc351832f3639868275621a9f93
cconner57 <chris.conner57@gmail.com>
2023-01-15 18:45:27 -0800

    cors update

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	"kanban/api/handler"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+        AllowOrigins: []string{"*"},
+        AllowMethods: []string{"POST", "PUT", "PATCH", "DELETE"},
+        AllowHeaders: []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
+    }))
 
 	router.GET("/", routes.GetTasks)
 
