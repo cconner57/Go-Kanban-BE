14b379bda624e3a19b8ea2757b4a775acecd9d1c
cconner57 <chris.conner57@gmail.com>
2023-03-27 16:46:45 -0700

    update cors

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	C "kanban/api/logic"
 
 	"github.com/go-chi/chi/v5"
-	// "github.com/go-chi/cors"
+	"github.com/go-chi/cors"
 
 	_ "github.com/lib/pq"
 )
@@ -19,14 +19,14 @@ func main() {
 
 	r := chi.NewRouter()
 	
-	// cors := cors.New(cors.Options{
-	// 	AllowedOrigins:   []string{"*"},
-	// 	// AllowCredentials: true,
-	// 	// AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
-	// 	AllowedHeaders:   []string{"access-control-allow-headers", "Content-Type", "access-control-allow-origin", "x-requested-with", "Origin", "Accept"},
-	// })
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"access-control-allow-headers", "Content-Type", "access-control-allow-origin", "x-requested-with", "Origin", "Accept"},
+	})
 
-	// r.Use(cors.Handler)
+	r.Use(cors.Handler)
 	r.Get("/", R.GetTasks)
 
 	http.ListenAndServe(":3000", r)
