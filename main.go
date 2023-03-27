89b028be3e9c1741e0eccb2ce9ab2f0dcf8f251e
cconner57 <chris.conner57@gmail.com>
2023-03-27 15:51:14 -0700

    disable cors

--- main.go
+++ main.go
@@ -19,14 +19,14 @@ func main() {
 
 	r := chi.NewRouter()
 	
-	cors := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		// AllowCredentials: true,
-		// AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
-		AllowedHeaders:   []string{"access-control-allow-headers", "Content-Type", "access-control-allow-origin", "x-requested-with", "Origin", "Accept"},
-	})
+	// cors := cors.New(cors.Options{
+	// 	AllowedOrigins:   []string{"*"},
+	// 	// AllowCredentials: true,
+	// 	// AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+	// 	AllowedHeaders:   []string{"access-control-allow-headers", "Content-Type", "access-control-allow-origin", "x-requested-with", "Origin", "Accept"},
+	// })
 
-	r.Use(cors.Handler)
+	// r.Use(cors.Handler)
 	r.Get("/", R.GetTasks)
 
 	http.ListenAndServe(":3000", r)
